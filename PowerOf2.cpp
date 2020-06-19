96ce5b1eb751fc283a7d1ddd45ab87fd243b60be
KasatNiraj <53268215+KasatNiraj@users.noreply.github.com>
2020-06-19 17:59:51 +0530

    Update PowerOf2.cpp

--- PowerOf2.cpp
+++ PowerOf2.cpp
@@ -1,12 +1,14 @@
 // Function to check power of two
 bool isPowerofTwo(long long n){
     
-    if(n==1)
-        return true;
+   if(n==0)
+        return false;
     //numbers in power of 2 have only 1 bit set
     //if we and it by substracting 1 from it
     //the obtained result will always be 0
-    //doesn't holds true in case of 1
+    //doesn't holds true in case of 0
+    //because 0 & -1 will throw error as 
+    //bitwise operators can't be used on negative numbers
     if(n&&!(n&(n-1)))  
         return true;
     return false;
